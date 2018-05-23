bbb1dde5ca62a5dc8e6c114e8ef137c0689fdd72
Sergio Manetta <sergiomanetta@yahoo.it>
2018-05-23 18:06:48 +0200

    Add handler for fetch exception

--- src/services/NotificationAPI.ts
+++ src/services/NotificationAPI.ts
@@ -11,6 +11,7 @@ import { ControllerError } from "../enums/ControllerError";
 import { NotificationSubscriptionRequestType } from "../enums/NotificationSubscriptionType";
 import { NotificationSubscriptionResponseAPI } from "../types/api/NotificationSubscriptionResponseAPI";
 import { FiscalCode } from "../types/FiscalCode";
+import { logger } from "../utils/Logger";
 
 // Update subscription (Activation or Deactivation) to Notification Service for a fiscalCode
 export async function updateNotificationsSubscription(
@@ -34,24 +35,29 @@ export async function updateNotificationsSubscription(
     }
   };
 
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" }
-  });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" }
+    });
 
-  if (!response.ok || response.status !== 200) {
+    if (!response.ok || response.status !== 200) {
+      return new Left(new Error(ControllerError.ERROR_PAGOPA_API_UNAVAILABLE));
+    }
+
+    const jsonResp = await response.json();
+    const errorOrNotificationResponse = NotificationSubscriptionResponseAPI.decode(
+      jsonResp
+    );
+    if (errorOrNotificationResponse.isLeft()) {
+      return new Left(new Error(ControllerError.ERROR_INVALID_API_RESPONSE));
+    }
+    return new Right(errorOrNotificationResponse.value);
+  } catch (err) {
+    logger.error(err);
     return new Left(new Error(ControllerError.ERROR_PAGOPA_API_UNAVAILABLE));
   }
-
-  const jsonResp = await response.json();
-  const errorOrNotificationResponse = NotificationSubscriptionResponseAPI.decode(
-    jsonResp
-  );
-  if (errorOrNotificationResponse.isLeft()) {
-    return new Left(new Error(ControllerError.ERROR_INVALID_API_RESPONSE));
-  }
-  return new Right(errorOrNotificationResponse.value);
 }
 
 enum OperationType {
