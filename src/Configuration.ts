85f9e62c67f9fc7dee4629e5250620c7da9fc411
Federico Feroldi <cloudify@users.noreply.github.com>
2019-05-27 15:17:59 +0200

    Use italia-tslint-rules (#216)

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -19,13 +19,14 @@ export const CONFIG = {
   // https://docs.google.com/document/d/1Qqe6mSfon-blHzc-ldeEHmzIkVaElKY5LtDnKiLbk80/edit
   // Used to expose services
   CONTROLLER: {
-    PORT: Number(process.env.PAGOPAPROXY_PORT) || 3000,
     HOST: process.env.PAGOPAPROXY_HOST || localhost,
+    PORT: Number(process.env.PAGOPAPROXY_PORT) || 3000,
     ROUTES: {
       RESTFUL: {
-        PAYMENT_REQUESTS_GET: "/payment-requests/:rptId",
+        PAYMENT_ACTIVATIONS_GET:
+          "/payment-activations/:codiceContestoPagamento",
         PAYMENT_ACTIVATIONS_POST: "/payment-activations",
-        PAYMENT_ACTIVATIONS_GET: "/payment-activations/:codiceContestoPagamento"
+        PAYMENT_REQUESTS_GET: "/payment-requests/:rptId"
       },
       SOAP: {
         PAYMENT_ACTIVATIONS_STATUS_UPDATE: "/FespCdService"
@@ -38,30 +39,30 @@ export const CONFIG = {
   // These values are the same for test and production environment
   // Used to communicate with PagoPA
   PAGOPA: {
-    HOST: process.env.PAGOPA_HOST || localhost,
-    PORT: Number(process.env.PAGOPA_PORT) || 3001,
-    HOST_HEADER: process.env.PAGOPA_HOST_HEADER,
     CLIENT_TIMEOUT_MSEC: Number(process.env.PAGOPA_TIMEOUT_MSEC) || 60000,
+    HOST: process.env.PAGOPA_HOST || localhost,
+    HOST_HEADER: process.env.PAGOPA_HOST_HEADER,
+    // These information will identify our system when it will access to PagoPA
+    IDENTIFIER: {
+      IDENTIFICATIVO_CANALE: process.env.PAGOPA_ID_CANALE,
+      IDENTIFICATIVO_CANALE_PAGAMENTO: process.env.PAGOPA_ID_CANALE_PAGAMENTO,
+      IDENTIFICATIVO_INTERMEDIARIO_PSP: process.env.PAGOPA_ID_INT_PSP,
+      IDENTIFICATIVO_PSP: process.env.PAGOPA_ID_PSP,
+      PASSWORD: process.env.PAGOPA_PASSWORD
+    },
+    PORT: Number(process.env.PAGOPA_PORT) || 3001,
     WS_SERVICES: {
       PAGAMENTI:
         process.env.PAGOPA_WS_URI ||
         "/webservices/pof/PagamentiTelematiciPspNodoservice"
-    },
-    // These information will identify our system when it will access to PagoPA
-    IDENTIFIER: {
-      IDENTIFICATIVO_PSP: process.env.PAGOPA_ID_PSP,
-      IDENTIFICATIVO_INTERMEDIARIO_PSP: process.env.PAGOPA_ID_INT_PSP,
-      IDENTIFICATIVO_CANALE: process.env.PAGOPA_ID_CANALE,
-      IDENTIFICATIVO_CANALE_PAGAMENTO: process.env.PAGOPA_ID_CANALE_PAGAMENTO,
-      PASSWORD: process.env.PAGOPA_PASSWORD
     }
   },
 
   // Redis DB Server Configuration
   REDIS_DB: {
-    PORT: Number(process.env.REDIS_DB_PORT) || 6379,
     HOST: process.env.REDIS_DB_URL || "redis://localhost",
     PASSWORD: process.env.REDIS_DB_PASSWORD || "ND",
+    PORT: Number(process.env.REDIS_DB_PORT) || 6379,
     USE_CLUSTER: Boolean(process.env.REDIS_USE_CLUSTER) || false
   },
 
@@ -72,8 +73,8 @@ export const CONFIG = {
 
 // Configuration validator - Define configuration types and interfaces
 const ServerConfiguration = t.interface({
-  PORT: WithinRangeNumber(0, 65535),
-  HOST: NonEmptyString
+  HOST: NonEmptyString,
+  PORT: WithinRangeNumber(0, 65535)
 });
 export type ServerConfiguration = t.TypeOf<typeof ServerConfiguration>;
 
@@ -82,9 +83,9 @@ const ControllerConfig = t.intersection([
   t.interface({
     ROUTES: t.interface({
       RESTFUL: t.interface({
-        PAYMENT_REQUESTS_GET: NonEmptyString,
+        PAYMENT_ACTIVATIONS_GET: NonEmptyString,
         PAYMENT_ACTIVATIONS_POST: NonEmptyString,
-        PAYMENT_ACTIVATIONS_GET: NonEmptyString
+        PAYMENT_REQUESTS_GET: NonEmptyString
       }),
       SOAP: t.interface({
         PAYMENT_ACTIVATIONS_STATUS_UPDATE: NonEmptyString
@@ -98,15 +99,15 @@ export const PagoPAConfig = t.intersection([
   ServerConfiguration,
   t.interface({
     CLIENT_TIMEOUT_MSEC: t.number,
-    WS_SERVICES: t.interface({
-      PAGAMENTI: NonEmptyString
-    }),
     IDENTIFIER: t.interface({
-      IDENTIFICATIVO_PSP: stText35_ppt,
-      IDENTIFICATIVO_INTERMEDIARIO_PSP: stText35_ppt,
       IDENTIFICATIVO_CANALE: stText35_ppt,
       IDENTIFICATIVO_CANALE_PAGAMENTO: stText35_ppt,
+      IDENTIFICATIVO_INTERMEDIARIO_PSP: stText35_ppt,
+      IDENTIFICATIVO_PSP: stText35_ppt,
       PASSWORD: stPassword_ppt
+    }),
+    WS_SERVICES: t.interface({
+      PAGAMENTI: NonEmptyString
     })
   }),
   t.partial({
@@ -116,9 +117,9 @@ export const PagoPAConfig = t.intersection([
 export type PagoPAConfig = t.TypeOf<typeof PagoPAConfig>;
 
 export const WinstonLogLevel = t.keyof({
+  debug: 4,
   error: 0,
-  info: 2,
-  debug: 4
+  info: 2
 });
 export type WinstonLogLevel = t.TypeOf<typeof WinstonLogLevel>;
 
@@ -132,10 +133,10 @@ export const RedisConfig = t.intersection([
 export type RedisConfig = t.TypeOf<typeof RedisConfig>;
 
 export const Configuration = t.interface({
-  WINSTON_LOG_LEVEL: WinstonLogLevel,
   CONTROLLER: ControllerConfig,
   PAGOPA: PagoPAConfig,
   PAYMENT_ACTIVATION_STATUS_TIMEOUT_SECONDS: t.number,
-  REDIS_DB: RedisConfig
+  REDIS_DB: RedisConfig,
+  WINSTON_LOG_LEVEL: WinstonLogLevel
 });
 export type Configuration = t.TypeOf<typeof Configuration>;
