11319e4026116345424d40b0ca5c255b0279dc13
Daniele Manni <dan.manni@gmail.com>
2020-07-20 11:14:23 +0200

    [#173889174] Optional APPINSIGHTS_INSTRUMENTATIONKEY env var

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 import * as appInsights from "applicationinsights";
 
+import { fromNullable } from "fp-ts/lib/Option";
 import { reporters } from "italia-ts-commons";
 import * as App from "./App";
 import { CONFIG, Configuration } from "./Configuration";
@@ -14,23 +15,26 @@ const config = Configuration.decode(CONFIG).getOrElseL(errors => {
   throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
 });
 
-// see https://github.com/projectkudu/kudu/wiki/Azure-runtime-environment#website-environment-variables
-const aiCloudRole = process.env.WEBSITE_SITE_NAME || "pagopa-proxy";
-logger.verbose(
-  `Starting application insights agent - cloudRole[${aiCloudRole}]`
-);
+// Initialize AppInsights if InstrumentationKey is provided
+fromNullable(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).map(k => {
+  // see https://github.com/projectkudu/kudu/wiki/Azure-runtime-environment#website-environment-variables
+  const aiCloudRole = process.env.WEBSITE_SITE_NAME || "pagopa-proxy";
+  logger.verbose(
+    `Starting application insights agent - cloudRole[${aiCloudRole}]`
+  );
 
-appInsights
-  .setup()
-  // do not buffer request data on disk
-  .setUseDiskRetryCaching(false);
+  appInsights
+    .setup(k)
+    // do not buffer request data on disk
+    .setUseDiskRetryCaching(false);
 
-// tslint:disable-next-line: no-object-mutation
-appInsights.defaultClient.context.tags[
-  appInsights.defaultClient.context.keys.cloudRole
-] = aiCloudRole;
+  // tslint:disable-next-line: no-object-mutation
+  appInsights.defaultClient.context.tags[
+    appInsights.defaultClient.context.keys.cloudRole
+  ] = aiCloudRole;
 
-appInsights.start();
+  appInsights.start();
+});
 
 // Define and start server
 App.startApp(config).catch(error => {
