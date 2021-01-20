bd95a342267bad8d2115902597ac51029a5768b7
gquadrati <greta.quadrati@pagopa.it>
2021-01-20 20:41:36 +0100

    [#176013578] added /info endopoint for healtcheck

--- src/App.ts
+++ src/App.ts
@@ -23,6 +23,12 @@ import * as FespCdServer from "./services/pagopa_api/FespCdServer";
 import * as PPTPortClient from "./services/pagopa_api/PPTPortClient";
 import { logger } from "./utils/Logger";
 
+import * as packageJson from "../package.json";
+
+interface IInfo {
+  version: string;
+}
+
 /**
  * Define a Service Handler for FespCdService SOAP service
  * It's an endpoint for PagoPA to confirm a payment activation result
@@ -203,6 +209,16 @@ function setRestfulRoutes(
   app.get("/ping", (_, res) => res.status(200).send("ok"));
 
   app.get("/", (_, res) => res.status(200).send());
+
+  //
+  // Healthcheck endpoint
+  //
+  app.get("/info", (_, res) => {
+    const info = {
+      version: packageJson.version
+    } as IInfo;
+    return info;
+  });
 }
 
 /**
