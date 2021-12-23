a438ac7f11d079020e742a022dbed8100a376aba
Giovanni Berti <giovanni.berti@pagopa.it>
2021-12-23 11:58:43 +0100

    Update `fp-ts` usages in `MockedData.ts`

--- src/utils/__tests__/MockedData.ts
+++ src/utils/__tests__/MockedData.ts
@@ -1,16 +1,18 @@
-import { RptId } from "italia-pagopa-commons/lib/pagopa";
-import { reporters } from "italia-ts-commons";
+import { RptId } from "@pagopa/io-pagopa-commons/lib/pagopa";
+import { reporters } from "@pagopa/ts-commons";
+import { pipe } from "fp-ts/lib/function";
 import { CodiceContestoPagamento } from "../../../generated/api/CodiceContestoPagamento";
 import { PaymentActivationsPostRequest } from "../../../generated/api/PaymentActivationsPostRequest";
-import { ctSpezzoniCausaleVersamento_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/ctSpezzoniCausaleVersamento_ppt";
-import { esitoNodoAttivaRPTRisposta_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/esitoNodoAttivaRPTRisposta_ppt";
-import { esitoNodoVerificaRPTRisposta_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/esitoNodoVerificaRPTRisposta_ppt";
+import { ctSpezzoniCausaleVersamento_type_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/ctSpezzoniCausaleVersamento_type_ppt";
+import { esitoNodoAttivaRPTRisposta_type_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/esitoNodoAttivaRPTRisposta_type_ppt";
+import { esitoNodoVerificaRPTRisposta_type_ppt } from "../../../generated/PagamentiTelematiciPspNodoservice/esitoNodoVerificaRPTRisposta_type_ppt";
 import { PagoPAConfig } from "../../Configuration";
+import * as E from "fp-ts/Either";
 
 // tslint:disable:no-identical-functions
 
-export const aVerificaRPTOutput = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutput = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "OK",
     datiPagamentoPA: {
       importoSingoloVersamento: 150.98,
@@ -34,17 +36,17 @@ export const aVerificaRPTOutput = esitoNodoVerificaRPTRisposta_ppt
       credenzialiPagatore: "NOMECOGNOME",
       causaleVersamento: "CAUSALE01"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOGeneric = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOGeneric = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -52,17 +54,17 @@ export const aVerificaRPTOutputKOGeneric = esitoNodoVerificaRPTRisposta_ppt
       faultString: "Identificativo richiedente non valido.",
       originalFaultCode: "PAA_CANALE_RICHIEDENTE_ERRATO"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOShort = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOShort = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -71,17 +73,17 @@ export const aVerificaRPTOutputKOShort = esitoNodoVerificaRPTRisposta_ppt
         "Pagamento PAA_PAGAMENTO_DUPLICATO in attesa risulta concluso all’Ente Creditore.",
       faultString: "Errore restituito dall’ente creditore"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
-
-export const aVerificaRPTOutputKOCompleted = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+  }));
+    
+export const aVerificaRPTOutputKOCompleted = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -90,17 +92,17 @@ export const aVerificaRPTOutputKOCompleted = esitoNodoVerificaRPTRisposta_ppt
       faultString: "Errore restituito dall’ente creditore",
       originalFaultCode: "PAA_PAGAMENTO_DUPLICATO"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOOnGoing = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOOnGoing = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -108,17 +110,17 @@ export const aVerificaRPTOutputKOOnGoing = esitoNodoVerificaRPTRisposta_ppt
       faultString: "Pagamento in attesa risulta in corso all’Ente Creditore.",
       originalFaultCode: "PAA_PAGAMENTO_IN_CORSO"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOExpired = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOExpired = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -126,34 +128,34 @@ export const aVerificaRPTOutputKOExpired = esitoNodoVerificaRPTRisposta_ppt
       faultString: "Pagamento in attesa risulta scaduto all’Ente Creditore.",
       originalFaultCode: "PAA_PAGAMENTO_SCADUTO"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aActivateKO = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aActivateKO = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
       faultCode: "PPT_SINTASSI_EXTRAXSD",
       faultString: "Errore sintassi "
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const anAttivaRPTOutput = esitoNodoAttivaRPTRisposta_ppt
-  .decode({
+export const anAttivaRPTOutput = pipe(
+  esitoNodoAttivaRPTRisposta_type_ppt.decode({
     esito: "OK",
     datiPagamentoPA: {
       importoSingoloVersamento: 99.05,
@@ -177,17 +179,17 @@ export const anAttivaRPTOutput = esitoNodoAttivaRPTRisposta_ppt
       credenzialiPagatore: "NOMECOGNOME",
       causaleVersamento: "CAUSALE01"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoAttivaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoAttivaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aAttivaRPTOutputKOAmount = esitoNodoAttivaRPTRisposta_ppt
-  .decode({
+export const aAttivaRPTOutputKOAmount = pipe(
+  esitoNodoAttivaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -196,32 +198,32 @@ export const aAttivaRPTOutputKOAmount = esitoNodoAttivaRPTRisposta_ppt
         "L’importo del pagamento in attesa non è congruente con il dato indicato dal PSP",
       originalFaultCode: "PAA_ATTIVA_RPT_IMPORTO_NON_VALIDO"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoAttivaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoAttivaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aAttivaRPTOutputKOGeneric = esitoNodoAttivaRPTRisposta_ppt
-  .decode({
+export const aAttivaRPTOutputKOGeneric = pipe(
+  esitoNodoAttivaRPTRisposta_type_ppt.decode({
     esito: "KO",
     faultCode: "PAA_TIPOFIRMA_SCONOSCIUTO",
     faultString: "Il campo tipoFirma non corrisponde ad alcun valore previsto.",
     originalFaultCode: "PAA_TIPOFIRMA_SCONOSCIUTO"
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoAttivaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoAttivaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOPASystemError = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOPASystemError = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -229,17 +231,17 @@ export const aVerificaRPTOutputKOPASystemError = esitoNodoVerificaRPTRisposta_pp
       faultString: "Errore generico lato PA.",
       originalFaultCode: "PAA_SYSTEM_ERROR"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
-export const aVerificaRPTOutputKOPAUnknownError = esitoNodoVerificaRPTRisposta_ppt
-  .decode({
+export const aVerificaRPTOutputKOPAUnknownError = pipe(
+  esitoNodoVerificaRPTRisposta_type_ppt.decode({
     esito: "KO",
     fault: {
       id: "NodoDeiPagamentiSPC",
@@ -247,14 +249,14 @@ export const aVerificaRPTOutputKOPAUnknownError = esitoNodoVerificaRPTRisposta_p
       faultString: "Errore generico lato PA.",
       originalFaultCode: "PAA_UNK_ERROR"
     }
-  })
-  .getOrElseL(errors => {
+  }),
+  E.getOrElseW(errors => {
     throw Error(
-      `Invalid esitoNodoVerificaRPTRisposta_ppt to decode: ${reporters.readableReport(
+      `Invalid esitoNodoVerificaRPTRisposta_type_ppt to decode: ${reporters.readableReport(
         errors
       )}`
     );
-  });
+  }));
 
 export const aCodiceContestoPagamento: CodiceContestoPagamento = "8447a9f0746811e89a8d5d4209060ab0" as CodiceContestoPagamento;
 
@@ -265,128 +267,148 @@ export const checkDigit = "12";
 export const segregationCode = "12";
 export const applicationCode = "12";
 
-export const aRptId0 = RptId.decode({
-  organizationFiscalCode: "12345678901",
-  paymentNoticeNumber: {
-    applicationCode,
-    auxDigit: "0",
-    checkDigit,
-    iuv13
-  }
-}).getOrElseL(errors => {
-  throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
-});
+export const aRptId0 = pipe(
+  RptId.decode({
+    organizationFiscalCode: "12345678901",
+    paymentNoticeNumber: {
+      applicationCode,
+      auxDigit: "0",
+      checkDigit,
+      iuv13
+    }
+  }),
+  E.getOrElseW(errors => {
+    throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
+  }));
 
-export const aRptId1 = RptId.decode({
-  organizationFiscalCode: "12345678901",
-  paymentNoticeNumber: {
-    auxDigit: "1",
-    iuv17
-  }
-}).getOrElseL(errors => {
-  throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
-});
-
-export const aRptId2 = RptId.decode({
-  organizationFiscalCode: "12345678901",
-  paymentNoticeNumber: {
-    auxDigit: "2",
-    checkDigit,
-    iuv15
-  }
-}).getOrElseL(errors => {
-  throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
-});
-
-export const aRptId3 = RptId.decode({
-  organizationFiscalCode: "12345678901",
-  paymentNoticeNumber: {
-    auxDigit: "3",
-    checkDigit,
-    iuv13,
-    segregationCode
-  }
-}).getOrElseL(errors => {
-  throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
-});
-
-export const aPaymentActivationsPostRequest = PaymentActivationsPostRequest.decode(
-  {
-    rptId: "12345678901012123456789012312",
-    importoSingoloVersamento: 9905,
-    codiceContestoPagamento: aCodiceContestoPagamento
-  }
-).getOrElseL(errors => {
-  throw Error(
-    `Invalid PaymentActivationsPostRequest to decode: ${reporters.readableReport(
-      errors
-    )}`
+export const aRptId1 = pipe(
+    RptId.decode({
+      organizationFiscalCode: "12345678901",
+      paymentNoticeNumber: {
+        auxDigit: "1",
+        iuv17
+      }
+    }),
+    E.getOrElseW(errors => {
+      throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
+    })
   );
-});
+
+export const aRptId2 = pipe(
+  RptId.decode({
+    organizationFiscalCode: "12345678901",
+    paymentNoticeNumber: {
+      auxDigit: "2",
+      checkDigit,
+      iuv15
+    }
+  }),
+  E.getOrElseW(errors => {
+    throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
+  })
+);
+
+export const aRptId3 = pipe(
+  RptId.decode({
+    organizationFiscalCode: "12345678901",
+    paymentNoticeNumber: {
+      auxDigit: "3",
+      checkDigit,
+      iuv13,
+      segregationCode
+    }
+  }),
+  E.getOrElseW(errors => {
+    throw Error(`Invalid RptId to decode: ${reporters.readableReport(errors)}`);
+  })
+);
+
+export const aPaymentActivationsPostRequest = pipe(
+  PaymentActivationsPostRequest.decode(
+    {
+      rptId: "12345678901012123456789012312",
+      importoSingoloVersamento: 9905,
+      codiceContestoPagamento: aCodiceContestoPagamento
+    }
+  ),
+  E.getOrElseW(errors => {
+    throw Error(
+      `Invalid PaymentActivationsPostRequest to decode: ${reporters.readableReport(
+        errors
+      )}`
+    );
+  })
+);
 
 // Define a PagoPA Configuration for tests
-export const aConfig = PagoPAConfig.decode({
-  HOST: process.env.PAGOPA_HOST || "http://localhost",
-  PORT: process.env.PAGOPA_PORT || 3001,
-  CERT: process.env.PAGOPA_CERT || "fakecert",
-  KEY: process.env.PAGOPA_KEY || "fakekey",
-  CLIENT_TIMEOUT_MSEC: process.env.PAGOPA_TIMEOUT_MSEC || 60000,
-  WS_SERVICES: {
-    PAGAMENTI: "/PagamentiTelematiciPspNodoservice/"
-  },
-  IDENTIFIER: {
-    IDENTIFICATIVO_PSP: "AGID_01",
-    IDENTIFICATIVO_INTERMEDIARIO_PSP: "97735020584",
-    IDENTIFICATIVO_CANALE: "97735020584_02",
-    IDENTIFICATIVO_CANALE_PAGAMENTO: "97735020584_xx",
-    PASSWORD: process.env.PAGOPA_PASSWORD || "nopassword"
-  },
-  APPINSIGHTS_DISABLE: "true",
-  APPINSIGHTS_SAMPLING_PERCENTAGE: "100",
-  APPINSIGHTS_INSTRUMENTATIONKEY: "key",
-  NM3_ENABLED: true
-}).getOrElseL(errors => {
-  throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
-});
-
-export const aSpezzoniCausaleVersamento = ctSpezzoniCausaleVersamento_ppt
-  .decode({
-    spezzoniCausaleVersamento: ["RATA GENNAIO", "RATA FEBBRAIO"]
-  })
-  .getOrElseL(errors => {
+export const aConfig = pipe(
+  PagoPAConfig.decode({
+    HOST: process.env.PAGOPA_HOST || "http://localhost",
+    PORT: process.env.PAGOPA_PORT || 3001,
+    CERT: process.env.PAGOPA_CERT || "fakecert",
+    KEY: process.env.PAGOPA_KEY || "fakekey",
+    CLIENT_TIMEOUT_MSEC: process.env.PAGOPA_TIMEOUT_MSEC || 60000,
+    WS_SERVICES: {
+      PAGAMENTI: "/PagamentiTelematiciPspNodoservice/"
+    },
+    IDENTIFIER: {
+      IDENTIFICATIVO_PSP: "AGID_01",
+      IDENTIFICATIVO_INTERMEDIARIO_PSP: "97735020584",
+      IDENTIFICATIVO_CANALE: "97735020584_02",
+      IDENTIFICATIVO_CANALE_PAGAMENTO: "97735020584_xx",
+      PASSWORD: process.env.PAGOPA_PASSWORD || "nopassword"
+    },
+    APPINSIGHTS_DISABLE: "true",
+    APPINSIGHTS_SAMPLING_PERCENTAGE: "100",
+    APPINSIGHTS_INSTRUMENTATIONKEY: "key",
+    NM3_ENABLED: true
+  }),
+  E.getOrElseW(errors => {
     throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
-  });
-
-export const aSpezzoniCausaleVersamentoStrutturato = ctSpezzoniCausaleVersamento_ppt
-  .decode({
-    spezzoniStrutturatoCausaleVersamento: [
-      {
-        causaleSpezzone: "RATA 1",
-        importoSpezzone: 1000
-      },
-      {
-        causaleSpezzone: "RATA 2",
-        importoSpezzone: 500
-      }
-    ]
   })
-  .getOrElseL(errors => {
-    throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
-  });
+);
 
-export const aSpezzoniCausaleVersamentoStrutturatoForController = ctSpezzoniCausaleVersamento_ppt
-  .decode({
-    spezzoniStrutturatoCausaleVersamento: [
-      {
-        causaleSpezzone: "RATA 1",
-        importoSpezzone: 100000
-      },
-      {
-        causaleSpezzone: "RATA 2",
-        importoSpezzone: 50000
-      }
-    ]
-  })
-  .getOrElseL(errors => {
-    throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
-  });
+  export const aSpezzoniCausaleVersamento = pipe(
+    ctSpezzoniCausaleVersamento_type_ppt.decode({
+      spezzoniCausaleVersamento: ["RATA GENNAIO", "RATA FEBBRAIO"]
+    }),
+    E.getOrElseW(errors => {
+      throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
+    })
+  );
+
+  export const aSpezzoniCausaleVersamentoStrutturato = pipe(
+      ctSpezzoniCausaleVersamento_type_ppt.decode({
+        spezzoniStrutturatoCausaleVersamento: [
+          {
+            causaleSpezzone: "RATA 1",
+            importoSpezzone: 1000
+          },
+          {
+            causaleSpezzone: "RATA 2",
+            importoSpezzone: 500
+          }
+        ]
+      }),
+      E.getOrElseW(errors => {
+        throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
+      })
+    );
+
+export const aSpezzoniCausaleVersamentoStrutturatoForController = pipe(ctSpezzoniCausaleVersamento_type_ppt
+    .decode({
+      spezzoniStrutturatoCausaleVersamento: [
+        {
+          causaleSpezzone: "RATA 1",
+          importoSpezzone: 100000
+        },
+        {
+          causaleSpezzone: "RATA 2",
+          importoSpezzone: 50000
+        }
+      ]
+    }),
+    E.getOrElseW(errors => {
+      throw Error(`Invalid configuration: ${reporters.readableReport(errors)}`);
+    })
+  );
