88944175b09935cc68544e48b94d35b3ab2ccc54
Giovanni Berti <giovanni.berti@pagopa.it>
2022-05-12 16:02:14 +0200

    Refactor PaymentFault -> Response utility function

--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -2,22 +2,29 @@ import {
   HttpStatusCodeEnum,
   ResponseErrorGeneric
 } from "@pagopa/ts-commons/lib/responses";
-import { PartyConfigurationFaultEnum } from "../../generated/api/PartyConfigurationFault";
-import { PartyTimeoutFaultEnum } from "../../generated/api/PartyTimeoutFault";
+import * as t from "io-ts";
+import * as E from "fp-ts/lib/Either";
+import * as A from "fp-ts/lib/Array";
+import { flow, pipe } from "fp-ts/lib/function";
+import { PartyConfigurationFault } from "../../generated/api/PartyConfigurationFault";
+import { PartyTimeoutFault } from "../../generated/api/PartyTimeoutFault";
 import { PaymentFaultEnum } from "../../generated/api/PaymentFault";
 import { PaymentFaultV2Enum } from "../../generated/api/PaymentFaultV2";
-import { GatewayFaultEnum } from "../../generated/api/GatewayFault";
-import { ValidationFaultEnum } from "../../generated/api/ValidationFault";
-import { PaymentStatusFaultEnum } from "../../generated/api/PaymentStatusFault";
+import { GatewayFault } from "../../generated/api/GatewayFault";
 import {
+  ValidationFault,
+  ValidationFaultEnum
+} from "../../generated/api/ValidationFault";
+import { PaymentStatusFault } from "../../generated/api/PaymentStatusFault";
+import {
+  IResponseErrorValidationFault,
   IResponseGatewayError,
   IResponseGatewayTimeout,
   IResponsePartyConfigurationError,
-  IResponseErrorValidationFault,
+  IResponsePaymentStatusFaultError,
   ResponseGatewayTimeout,
   ResponsePartyConfigurationError,
   ResponsePaymentError,
-  IResponsePaymentStatusFaultError,
   ResponsePaymentStatusFaultError
 } from "./types";
 import { logger } from "./Logger";
@@ -40,61 +47,62 @@ export const ResponseErrorValidationFault: (
   };
 };
 
-export const responseFromPaymentFault: (
-  detail: PaymentFaultEnum,
-  detail_v2: PaymentFaultV2Enum
-) =>
+type FaultResponse =
   | IResponsePaymentStatusFaultError
   | IResponsePartyConfigurationError
   | IResponseErrorValidationFault
   | IResponseGatewayError
-  | IResponseGatewayTimeout = (detail, detail_v2) => {
-  if (
-    Object.values(PartyConfigurationFaultEnum).includes(
-      (detail_v2 as unknown) as PartyConfigurationFaultEnum
+  | IResponseGatewayTimeout;
+
+export const responseFromPaymentFault: (
+  detail: PaymentFaultEnum,
+  detail_v2: PaymentFaultV2Enum
+) => FaultResponse = (detail, detail_v2) => {
+  // eslint-disable-next-line functional/prefer-readonly-type
+  const parsers: Array<t.Decode<PaymentFaultV2Enum, FaultResponse>> = [
+    flow(
+      PartyConfigurationFault.decode,
+      E.map(v => ResponsePartyConfigurationError(detail, v))
+    ),
+    flow(
+      ValidationFault.decode,
+      E.map(v => ResponseErrorValidationFault(detail, v))
+    ),
+    flow(
+      PaymentStatusFault.decode,
+      E.map(v => ResponsePaymentStatusFaultError(detail, v))
+    ),
+    flow(
+      GatewayFault.decode,
+      E.map(v => ResponsePaymentError(detail, v))
+    ),
+    flow(
+      PartyTimeoutFault.decode,
+      E.map(v => ResponseGatewayTimeout(v))
     )
-  ) {
-    return ResponsePartyConfigurationError(
-      detail,
-      (detail_v2 as unknown) as PartyConfigurationFaultEnum
+  ];
+
+  const reducer: (
+    current: E.Either<FaultResponse, PaymentFaultV2Enum>,
+    decoder: t.Decode<PaymentFaultV2Enum, FaultResponse>
+  ) => E.Either<FaultResponse, PaymentFaultV2Enum> = (current, decoder) =>
+    pipe(
+      current,
+      E.chain((d: PaymentFaultV2Enum) =>
+        pipe(
+          decoder(d),
+          E.swap,
+          E.map(_ => d)
+        )
+      )
     );
-  } else if (
-    Object.values(ValidationFaultEnum).includes(
-      (detail_v2 as unknown) as ValidationFaultEnum
-    )
-  ) {
-    return ResponseErrorValidationFault(
-      detail,
-      (detail_v2 as unknown) as ValidationFaultEnum
-    );
-  } else if (
-    Object.values(PaymentStatusFaultEnum).includes(
-      (detail_v2 as unknown) as PaymentStatusFaultEnum
-    )
-  ) {
-    return ResponsePaymentStatusFaultError(
-      detail,
-      (detail_v2 as unknown) as PaymentStatusFaultEnum
-    );
-  } else if (
-    Object.values(GatewayFaultEnum).includes(
-      (detail_v2 as unknown) as GatewayFaultEnum
-    )
-  ) {
-    return ResponsePaymentError(
-      detail,
-      (detail_v2 as unknown) as GatewayFaultEnum
-    );
-  } else if (
-    Object.values(PartyTimeoutFaultEnum).includes(
-      (detail_v2 as unknown) as PartyTimeoutFaultEnum
-    )
-  ) {
-    return ResponseGatewayTimeout(
-      (detail_v2 as unknown) as PartyTimeoutFaultEnum
-    );
-  } else {
-    logger.error(`unmapped detail_v2: ${detail_v2}`);
-    throw new Error(`unmapped detail_v2: ${detail_v2}`);
-  }
+
+  return pipe(
+    parsers,
+    A.reduce(E.right(detail_v2), reducer),
+    E.fold(t.identity, (variant: PaymentFaultV2Enum) => {
+      logger.error(`unmapped detail_v2: ${variant}`);
+      throw new Error(`unmapped detail_v2: ${variant}`);
+    })
+  );
 };
