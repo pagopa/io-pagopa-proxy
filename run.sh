f4a64d5583152306c8d4f397d1022190f577399d
Jacopo <jacopo.carlini@gmail.com>
2022-11-24 18:08:47 +0100

    github action

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
-# 1. create config-prod.yaml
+# 1. creare config-prod.yaml
 # 2. lanciare lo script che crea la cartella azure-dashboard
-# 2. init cartella env dentro /azure-dashboard
-# 3. init cartella .identity
-
-# shellcheck disable=SC2046
 docker run -v $(pwd):/home/nonroot/myfolder:Z \
-  ghcr.io/pagopa/opex-dashboard:latest generate \
+  ghcr.io/pagopa/opex-dashboard:v1.4.4 generate \
   --template-name azure-dashboard \
   --config-file myfolder/monitor/config-"$1".yaml \
   --package myfolder
 
+# 3. init cartella env dentro /azure-dashboard
 cd ./azure-dashboard || exit
 sh terraform.sh apply "$1"
+
+# 3. init cartella .identity
+
+
+
+docker run -v $(pwd):/home/nonroot/myfolder:Z \
+  ghcr.io/pagopa/opex-dashboard:v1.4.6 generate \
+  --template-name azure-dashboard \
+  --config-file myfolder/monitor/config-prod.yaml \
+  --package myfolder
