5d13af660c7747277c15761da53d39268443a7c1
Jacopo Carlini <jacopo.carlini@gmail.com>
2022-12-13 11:54:20 +0100

    Add OE dashboard action (#365)
    * opex
    
    * identity
    
    * fix identity
    
    * github action
    
    * added create_dashboard.yaml
    
    * added create_dashboard.yaml
    
    * trigger
    
    * trigger
    
    * trigger
    
    * fix action
    
    * bash
    
    * bash
    
    * bash
    
    * bash
    
    * try
    
    * try
    
    * try
    
    * try
    
    * permission
    
    * try docker run
    
    * fix
    
    * fix
    
    * root
    
    * action
    
    * action
    
    * action
    
    * action
    
    * action
    
    * rollback --force-with-lease
    
    * fix
    
    * fix
    
    * new trigger
    
    * fix bash
    
    * fix
    
    Co-authored-by: pasqualedevita <>

---
+++ .identity/02_add_permissions.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+ENV=$1
+
+ENV_FILE=".$ENV.env"
+
+if [ -z "$ENV" ]; then
+  echo "env should be: dev, uat or prod."
+  exit 1
+fi
+
+if [[ ! -f ${ENV_FILE} ]]
+then
+    echo "${ENV_FILE} does not exist"
+    exit 1
+fi
+
+source "${ENV_FILE}"
+
+az account set -s "${SUBSCRIPTION_NAME}"
+ACCOUNT_INFO=$(az account show 2> /dev/null)
+SUBSCRIPTION_ID=$(echo "$ACCOUNT_INFO" | jq ".id" -r)
+
+APP_NAME="github-${GITHUB_REPO_ORG}-${GITHUB_REPO_NAME}-${GITHUB_REPO_BRANCH}-sp"
+
+echo "[INFO] Get service principal APP_NAME: ${APP_NAME}"
+SP_OBJECT_ID=$(az ad sp list --all -o tsv --query "[?contains(displayName,'$APP_NAME')].{Name:id}")
+
+echo "[INFO] Remove role assignment SP_OBJECT_ID: ${SP_OBJECT_ID}"
+az role assignment delete \
+  --subscription="${SUBSCRIPTION_ID}" \
+  --assignee "${SP_OBJECT_ID}"
+
+az role assignment create \
+  --role contributor \
+  --subscription "${SUBSCRIPTION_ID}" \
+  --assignee-object-id "${SP_OBJECT_ID}" \
+  --assignee-principal-type ServicePrincipal \
+  --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/io-infra-rg"
+
+az role assignment create \
+  --role contributor \
+  --subscription "${SUBSCRIPTION_ID}" \
+  --assignee-object-id "${SP_OBJECT_ID}" \
+  --assignee-principal-type ServicePrincipal \
+  --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/dashboards"
+
+az role assignment create \
+  --role reader \
+  --subscription "${SUBSCRIPTION_ID}" \
+  --assignee-object-id "${SP_OBJECT_ID}" \
+  --assignee-principal-type ServicePrincipal \
+  --scope "/subscriptions/${SUBSCRIPTION_ID}"
+
